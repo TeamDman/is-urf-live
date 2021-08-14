5dffac4a586a2b8f4c1a30d9e2524784949d221a
TeamDman <TeamDman9201@gmail.com>
2021-08-14 18:27:18 -0400

    Add more diag to upload

--- upload.ps1
+++ upload.ps1
@@ -1,5 +1,7 @@
+Write-Host "Reading config";
 $config = Get-Content .env | ConvertFrom-StringData;
 
+Write-Host "Uploading content";
 az storage blob upload `
     --account-name $config.STORAGE_ACCOUNT_NAME `
     --account-key $config.STORAGE_ACCOUNT_ACCESS_KEY `

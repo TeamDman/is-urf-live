f6fa1492f6439348a2e44f41a77b2a93492d0fe0
TeamDman <TeamDman9201@gmail.com>
2021-08-14 23:22:38 -0400

    Organize files

---
+++ 1-launch.ps1
@@ -0,0 +1,33 @@
+# Use Live
+# $leagueClientPath = "C:\Riot Games\League of Legends\LeagueClient.exe";
+
+# Use PBE
+$leagueClientPath = "C:\Riot Games\League of Legends (PBE)\LeagueClient.exe";
+
+# Start the league client in the background
+$riotClient = Start-Process `
+    -FilePath $leagueClientPath `
+    -PassThru;
+
+# Run the node script (blocking) that fetches the current gamemodes
+ts-node .\app\index.ts;
+
+# Script has exited and writen the gamemodes.json file
+# If the script exits with 0, then URF is in the list of gamemodes
+$isUrfLive=$?;
+
+# Close the league client
+Stop-Process $riotClient;
+Stop-Process -Name LeagueClient;
+
+# Play the urf mp3 if it's available.
+if ($isUrfLive) {
+    .\resources\urf.mp3;
+}
+
+# Run the upload script to make the json available online for others
+.\2-upload.ps1;
+
+# Wait for user acknowledgement before closing dialog
+# Write-Host -NoNewLine 'Press any key to continue...';
+# $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,9 @@
 import { authenticate, Credentials, request } from "league-connect";
 import { writeFileSync } from "fs";
+
+/**
+ * Uses the LCU API to get list of active game modes.
+ */
 async function getGameModes(credentials: Credentials): Promise<Array<any>> {
     console.log(`Fetching gamemodes`);
     const response = await request({
@@ -11,33 +15,51 @@ async function getGameModes(credentials: Credentials): Promise<Array<any>> {
     return gamemodes;
 }
 
+/**
+ * Sleeps for the desired delay.
+ */
 async function sleep(ms:number): Promise<void> {
     return new Promise((res, rej) => {
         setTimeout(res, ms);
     });
 }
 
+/**
+ * Main body
+ */
 async function main() {
+    // Waits for client to be logged in
     console.log(`Fetching credentials`);
     const credentials = await authenticate({
         awaitConnection: true,
         pollInterval: 5000
     });
+
+    // Once logged in, start polling the gamemode endpoint
     for(let attempt=0; attempt<500; attempt++) {
         console.log(`Attempt ${attempt}`);
         try {
+            // Get gamemodes
             const gameModes = await getGameModes(credentials);
+
+            // Display in console
             console.dir(gameModes);
+
+            // Write the JSON file
             writeFileSync("./gamemodes.json", JSON.stringify({
                 dateGenerated: new Date(),
                 gameModes: gameModes.map(x => ({id: x.gameMode, name: x.name}))
             }, null, 4));
+
+            // Exit with success(0) if URF is in the list, error(1) otherwise
             process.exit(gameModes.some(x => x.gameMode === "URF") ? 0 : 1);
         } catch (e) {
             console.log(e);
             await sleep(1000);
         }
     }
+
+    // Exit with error if gamemodes endpoint didn't respond.
     process.exit(1);
 }
 main();
